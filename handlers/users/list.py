896506af0969db6eca10f9a6bf895826a58265ae
kairoslav <svt210@yandex.ru>
2021-09-10 17:33:58 +0300

    Fixed: list.py last 10 places

--- handlers/users/list.py
+++ handlers/users/list.py
@@ -7,7 +7,7 @@ from utils.db_api.db_methods import get_user_places
 @dp.message_handler(commands=["list"])
 async def get_list(message: types.Message):
     user_places = get_user_places(message.from_user.id)
-    for user_place in user_places:
+    for user_place in user_places[-10:]:
         place = Place(*user_place[:4])
         await message.answer(text=f"{place.name}")
         await message.answer_location(place.latitude, place.longitude)
